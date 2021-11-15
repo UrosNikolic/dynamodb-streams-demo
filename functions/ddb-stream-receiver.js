cb380ea16d38de5f5b75e8bee001dc1b892cc834
urosnikolic <unikolic94@gmail.com>
2021-11-15 15:03:57 +0100

    Enable ddb table streams and trigger function on item added to ddb table

--- functions/ddb-stream-receiver.js
+++ functions/ddb-stream-receiver.js
@@ -1,4 +1,6 @@
 
 module.exports.ddbStreamReceiver = async (event) => {
-    // console.log('\nSTREAM RECORDS: ', JSON.stringify(event['Records'][0].dynamodb['NewImage']))
+    console.log('STREAMS ', JSON.stringify(event['Records']))
+    const records = event['Records'][0].dynamodb['NewImage']
+    console.log('\nSTREAM RECORDS: ', JSON.stringify(records))
 }
